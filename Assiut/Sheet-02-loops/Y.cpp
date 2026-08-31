db49969484ad5c69c1f76f3155719f202c8dd88c
aqdarahmad <a.abusufyan@student.aaup.edu>
2026-08-31 12:12:32 +0300

    easy fib

---
+++ Assiut/Sheet-02-loops/Y.cpp
@@ -0,0 +1,24 @@
+using namespace std;
+#include<bits/stdc++.h>
+
+
+
+int main()
+{
+long long n;
+cin>>n;
+=
+long long arr[n];
+arr[0]=0;
+arr[1]=1;
+for(int i=2;i<n;i++)
+ {
+    arr[i]=arr[i-1]+arr[i-2];
+ }
+ for(int x : arr)
+  cout<<x<<" ";
+
+ cout<<endl; 
+
+return 0;    
+}
