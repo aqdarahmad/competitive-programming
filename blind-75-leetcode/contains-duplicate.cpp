d7a530083e9cce36101e4aaeaa1286cf7bef1492
aqdarahmad <a.abusufyan@student.aaup.edu>
2026-09-08 18:17:21 +0300

    solve contain duplicate using set

---
+++ blind-75-leetcode/contains-duplicate.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        set<int>st;
+        for(int x:nums)
+         st.insert(x);
+
+
+      return st.size()!=nums.size();   
+        
+    }
+};
